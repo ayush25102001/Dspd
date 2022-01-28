b8004b78f28138f8838cbd89199be089f5255519
Ayush Kangane <78008646+ayush25102001@users.noreply.github.com>
2022-01-28 20:53:58 +0530

    Add files via upload

---
+++ STV_(Dp-6)-Grid-1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int compute(int i,int j,vector<vector<int> > &dp){
+        if((i==0)&&(j==0)){
+            return 1;
+        }
+        if((i<0)||(j<0)){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int up=compute(i-1,j,dp);
+        int left=compute(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
+int uniquePaths(int m, int n){
+	 vector<vector<int> > dp(m,vector<int>(n,-1));
+     return compute(m-1,n-1,dp);
+}
+int main(){
+	int m,n;
+	cin>>m>>n;
+	cout<<uniquePaths(m,n)<<endl;
+}
